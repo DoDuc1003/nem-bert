1287fc3c5d9602840cec5b1bba5de93729252203
DucDo <dominhduc1032002@gmail.com>
2025-04-28 22:05:06 +0700

    add load and read data wiki

--- example/train_roberta_mlm.py
+++ example/train_roberta_mlm.py
@@ -19,3 +19,23 @@ from tokenizers.processors import BertProcessing
 # 1. Setup logging
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+# 2. Load Wikipedia dataset
+dataset_checkpoint = "wikimedia/wikipedia"
+wiki = load_dataset(
+    dataset_checkpoint,
+    "20231101.vi",
+    trust_remote_code=True,
+    cache_dir=f'./cache/dataset/{dataset_checkpoint}'
+)
+
+# Save to plain text
+output_dir = Path("./data/wikipedia_vi")
+output_dir.mkdir(parents=True, exist_ok=True)
+output_txt_path = output_dir / "wiki_corpus.txt"
+
+with open(output_txt_path, "w", encoding="utf-8") as f:
+    for example in wiki["train"]:
+        text = example["text"].strip()
+        if text:
+            f.write(text.replace("\n", " ") + "\n")
